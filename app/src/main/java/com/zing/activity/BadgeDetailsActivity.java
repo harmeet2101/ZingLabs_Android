5efc93bb5001a579425e2a37279eac1bdbdcf559
Harmeet Singh <harmeet.singh1@mail.vinove.com>
2019-04-29 17:44:14 +0530

    badges ui and api impl

---
+++ app/src/main/java/com/zing/activity/BadgeDetailsActivity.java
@@ -0,0 +1,95 @@
+package com.zing.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.zing.R;
+import com.zing.base.BaseActivity;
+
+import androidx.annotation.Nullable;
+
+public class BadgeDetailsActivity extends BaseActivity implements View.OnClickListener {
+
+
+    ImageView closeImageview;
+    ImageView badgeImageView;
+    TextView tvBadgeInfo;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_badges_details);
+        closeImageview = (ImageView)findViewById(R.id.close);
+        badgeImageView = (ImageView)findViewById(R.id.badgeImage);
+        tvBadgeInfo = (TextView)findViewById(R.id.tvBadgeInfo);
+        closeImageview.setOnClickListener(this);
+
+        String badgeType = getIntent().getExtras().getString("badgeType");
+        setUpData(badgeType);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()){
+            case R.id.close:
+                finish();
+                break;
+        }
+    }
+
+    private  void setUpData(String type){
+
+        switch (type){
+
+            case "Completed":
+                tvBadgeInfo.setText("First Shift Completed");
+                badgeImageView.setImageDrawable(getResources().getDrawable(R.drawable.badge_first_shift_completed));
+                break;
+            case "OnTime_5":
+                tvBadgeInfo.setText("Show up on time");
+                badgeImageView.setImageDrawable(getResources().getDrawable(R.drawable.badge_show_up_on_time_5));
+                break;
+            case "OnTime_10":
+                tvBadgeInfo.setText("Show up on time");
+                badgeImageView.setImageDrawable(getResources().getDrawable(R.drawable.badge_show_up_on_time_10));
+                break;
+            case "OnTime_20":
+                tvBadgeInfo.setText("Show up on time");
+                badgeImageView.setImageDrawable(getResources().getDrawable(R.drawable.badge_show_up_on_time_20));
+                break;
+            case "OnTime_50":
+                tvBadgeInfo.setText("Show up on time");
+                badgeImageView.setImageDrawable(getResources().getDrawable(R.drawable.badge_show_up_on_time_50));
+                break;
+            case "OnTime_100":
+                tvBadgeInfo.setText("Show up on time");
+                badgeImageView.setImageDrawable(getResources().getDrawable(R.drawable.badge_show_up_on_time_100));
+                break;
+            case "Perfect_w":
+                tvBadgeInfo.setText("Perfect");
+                badgeImageView.setImageDrawable(getResources().getDrawable(R.drawable.badge_perfect_week));
+                break;
+
+            case "Perfect_m":
+                tvBadgeInfo.setText("Perfect");
+                badgeImageView.setImageDrawable(getResources().getDrawable(R.drawable.badge_perfect_month));
+                break;
+            case "Recommended_1":
+                tvBadgeInfo.setText("Recommended Shift");
+                badgeImageView.setImageDrawable(getResources().getDrawable(R.drawable.badge_picked_up_recommended_shifts));
+                break;
+            case "Recommended_5":
+                tvBadgeInfo.setText("Recommended Shift");
+                badgeImageView.setImageDrawable(getResources().getDrawable(R.drawable.badge_rec_5));
+                break;
+
+            case "Recommended_10":
+                tvBadgeInfo.setText("Recommended Shift");
+                badgeImageView.setImageDrawable(getResources().getDrawable(R.drawable.badge_rec_10));
+                break;
+        }
+    }
+}
--- app/src/main/java/com/zing/fragment/BadgesFragment.java
+++ app/src/main/java/com/zing/fragment/BadgesFragment.java
@@ -2,22 +2,19 @@ package com.zing.fragment;
 
 
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.v7.widget.GridLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
 import com.zing.R;
-import com.zing.adapter.BadgesAdapter;
-import com.zing.adapter.GridBadgesAdapter;
-import com.zing.model.response.StatsResponse.Badge;
+import com.zing.activity.BadgeDetailsActivity;
 import com.zing.util.AppTypeface;
 import com.zing.util.CommonUtils;
 import com.zing.util.NetworkUtils;
@@ -25,34 +22,62 @@ import com.zing.util.SessionManagement;
 import com.zing.util.restClient.ApiClient;
 import com.zing.util.restClient.ZinglabsApi;
 
-import org.json.JSONArray;
 import org.json.JSONObject;
-
-import java.util.ArrayList;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 import butterknife.Unbinder;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-public class BadgesFragment extends BaseFragment {
+public class BadgesFragment extends BaseFragment implements View.OnClickListener{
 
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
-    @BindView(R.id.tvBadges)
+    @BindView(R.id.firstShiftLayout)
+    ViewGroup firstShiftLayout;
+    @BindView(R.id.onTimeShiftLayout)
+    ViewGroup onTimeShiftLayout;
+    @BindView(R.id.perfectShiftLayout)
+    ViewGroup perfectShiftLayout;
+    @BindView(R.id.recommendShiftLayout)
+    ViewGroup recommendShiftLayout;
+    @BindView(R.id.ImageView_show_up_on_time_5)
+    ImageView imageView_show_up_on_time_5;
+    @BindView(R.id.ImageView_show_up_on_time_10)
+    ImageView imageView_show_up_on_time_10;
+    @BindView(R.id.ImageView_show_up_on_time_20)
+    ImageView imageView_show_up_on_time_20;
+    @BindView(R.id.ImageView_show_up_on_time_50)
+    ImageView imageView_show_up_on_time_50;
+    @BindView(R.id.ImageView_show_up_on_time_100)
+    ImageView imageView_show_up_on_time_100;
+    @BindView(R.id.ImageView_perfect_week)
+    ImageView imageView_perfect_week;
+    @BindView(R.id.ImageView_perfect_month)
+    ImageView imageView_perfect_month;
+
+    @BindView(R.id.ImageView_recommended_shift_1)
+    ImageView imageView_recommended_shift_1;
+    @BindView(R.id.ImageView_recommended_shift_5)
+    ImageView imageView_recommended_shift_5;
+    @BindView(R.id.ImageView_recommended_shift_10)
+    ImageView imageView_recommended_shift_10;
+
+
+
+    @BindView(R.id.tvHeading)
     TextView tvBadges;
-    @BindView(R.id.rvBadges)
-    RecyclerView rvBadges;
     Unbinder unbinder;
 
     private String mParam1;
     private String mParam2;
     private ProgressDialog progressDialog;
     SessionManagement session;
-    ArrayList<Badge> badgesList;
-    private GridBadgesAdapter myAdapter;
+    //ArrayList<Badge> badgesList;
+    //private GridBadgesAdapter myAdapter;
 
     public static BadgesFragment newInstance(String param1, String param2) {
         BadgesFragment fragment = new BadgesFragment();
@@ -74,7 +99,7 @@ public class BadgesFragment extends BaseFragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_badges, container, false);
+        View view = inflater.inflate(R.layout.layout_badges, container, false);
         unbinder = ButterKnife.bind(this, view);
         return view;
     }
@@ -86,13 +111,13 @@ public class BadgesFragment extends BaseFragment {
         AppTypeface.getTypeFace(getActivity());
         tvBadges.setTypeface(AppTypeface.avenieNext_demibold);
         session = new SessionManagement(getActivity());
-        badgesList = new ArrayList<>();
+        //badgesList = new ArrayList<>();
 
-        GridLayoutManager gridLayoutManager = new GridLayoutManager(getActivity(), 2);
+        /*GridLayoutManager gridLayoutManager = new GridLayoutManager(getActivity(), 2);
         rvBadges.setHasFixedSize(true);
         rvBadges.setLayoutManager(gridLayoutManager);
          myAdapter = new GridBadgesAdapter(getActivity(), badgesList);
-        rvBadges.setAdapter(myAdapter);
+        rvBadges.setAdapter(myAdapter);*/
 
     }
 
@@ -143,7 +168,98 @@ public class BadgesFragment extends BaseFragment {
                             int perfect_month = show_rate.optInt("perfect_month");
                             int recommended_shift_picked = badgesObj.optInt("recommended_shift_picked");
 
-                            /*JSONArray badgeArr = jsonObject.optJSONArray("badges");
+
+                            if(first_shift_completed==0 && show_up_on_time==0
+
+                            && perfect_week==0 && perfect_month==0
+                                    && recommended_shift_picked==0
+
+                            ){
+                                tvBadges.setText("No Badges Found");
+                            }
+                            if(first_shift_completed==0)
+                                firstShiftLayout.setVisibility(View.GONE);
+                            else
+                                firstShiftLayout.setVisibility(View.VISIBLE);
+
+                            if(show_up_on_time==0)
+                                onTimeShiftLayout.setVisibility(View.GONE);
+                            else
+                                onTimeShiftLayout.setVisibility(View.VISIBLE);
+
+                            if(recommended_shift_picked==0)
+                                recommendShiftLayout.setVisibility(View.GONE);
+                            else
+                                recommendShiftLayout.setVisibility(View.VISIBLE);
+
+                            if(perfect_week==0 && perfect_month ==0)
+                                perfectShiftLayout.setVisibility(View.GONE);
+                                else if(perfect_week==0 && perfect_month!=0){
+                                    perfectShiftLayout.setVisibility(View.VISIBLE);
+                                    imageView_perfect_week.setVisibility(View.GONE);
+                                    imageView_perfect_month.setVisibility(View.VISIBLE);
+
+                            }else if(perfect_month==0 && perfect_week!=0){
+                                perfectShiftLayout.setVisibility(View.VISIBLE);
+                                imageView_perfect_month.setVisibility(View.GONE);
+                                imageView_perfect_week.setVisibility(View.VISIBLE);
+                            }
+
+
+                            imageView_show_up_on_time_5.setVisibility(View.GONE);
+                            imageView_show_up_on_time_10.setVisibility(View.GONE);
+                            imageView_show_up_on_time_20.setVisibility(View.GONE);
+                            imageView_show_up_on_time_50.setVisibility(View.GONE);
+                            imageView_show_up_on_time_100.setVisibility(View.GONE);
+
+                            if(show_up_on_time < 10)
+                                imageView_show_up_on_time_5.setVisibility(View.VISIBLE);
+
+                            else if(show_up_on_time >=10 && show_up_on_time <= 20) {
+                                imageView_show_up_on_time_5.setVisibility(View.VISIBLE);
+                                imageView_show_up_on_time_10.setVisibility(View.VISIBLE);
+                            }
+                            else if(show_up_on_time >21 && show_up_on_time <49) {
+                                imageView_show_up_on_time_5.setVisibility(View.VISIBLE);
+                                imageView_show_up_on_time_10.setVisibility(View.VISIBLE);
+                                imageView_show_up_on_time_20.setVisibility(View.VISIBLE);
+                            }
+                            else  if(show_up_on_time >= 50 &&  show_up_on_time < 100) {
+                                imageView_show_up_on_time_5.setVisibility(View.VISIBLE);
+                                imageView_show_up_on_time_10.setVisibility(View.VISIBLE);
+                                imageView_show_up_on_time_20.setVisibility(View.VISIBLE);
+                                imageView_show_up_on_time_50.setVisibility(View.VISIBLE);
+                            }
+                            else  {
+                                imageView_show_up_on_time_5.setVisibility(View.VISIBLE);
+                                imageView_show_up_on_time_10.setVisibility(View.VISIBLE);
+                                imageView_show_up_on_time_20.setVisibility(View.VISIBLE);
+                                imageView_show_up_on_time_50.setVisibility(View.VISIBLE);
+                                imageView_show_up_on_time_100.setVisibility(View.VISIBLE);
+                            }
+
+
+                            imageView_recommended_shift_1.setVisibility(View.GONE);
+                            imageView_recommended_shift_5.setVisibility(View.GONE);
+                            imageView_recommended_shift_10.setVisibility(View.GONE);
+
+
+
+                            if(recommended_shift_picked > 0 && recommended_shift_picked < 5)
+                                imageView_recommended_shift_1.setVisibility(View.VISIBLE);
+
+                            else if(recommended_shift_picked >=5 && recommended_shift_picked <10) {
+                                imageView_recommended_shift_1.setVisibility(View.VISIBLE);
+                                imageView_recommended_shift_5.setVisibility(View.VISIBLE);
+                            }
+                            else if(recommended_shift_picked >=10) {
+                                imageView_recommended_shift_1.setVisibility(View.VISIBLE);
+                                imageView_recommended_shift_5.setVisibility(View.VISIBLE);
+                                imageView_recommended_shift_10.setVisibility(View.VISIBLE);
+                            }
+
+
+                           /*JSONArray badgeArr = jsonObject.optJSONArray("badges");
                             for (int i = 0; i < badgeArr.length(); i++) {
                                 JSONObject badgesObj = badgeArr.optJSONObject(i);
                                 Badge badge = new Badge();
@@ -156,7 +272,7 @@ public class BadgesFragment extends BaseFragment {
                                 badgesList.add(badge);
                             }*/
 
-                            myAdapter.notifyDataSetChanged();
+                            //myAdapter.notifyDataSetChanged();
 
                         } catch (Exception e) {
                             e.printStackTrace();
@@ -175,6 +291,79 @@ public class BadgesFragment extends BaseFragment {
         } catch (Exception e) {
             progressDialog.dismiss();
             e.printStackTrace();
+        }
+    }
+
+    @OnClick({R.id.firstShiftLayout, R.id.ImageView_show_up_on_time_5,R.id.ImageView_show_up_on_time_10
+              ,R.id.ImageView_show_up_on_time_20,R.id.ImageView_show_up_on_time_50,
+                R.id.ImageView_show_up_on_time_100,R.id.ImageView_perfect_week,R.id.ImageView_perfect_month,
+            R.id.ImageView_recommended_shift_1,R.id.ImageView_recommended_shift_5,R.id.ImageView_recommended_shift_10
+
+    })
+    @Override
+    public void onClick(View v) {
+
+        Intent intent = new Intent(getActivity(),BadgeDetailsActivity.class);
+        Bundle bundle = new Bundle();
+        switch (v.getId()){
+
+            case R.id.firstShiftLayout:
+                bundle.putString("badgeType","Completed");
+                intent.putExtras(bundle);
+                getContext().startActivity(intent);
+                break;
+            case  R.id.ImageView_show_up_on_time_5:
+                bundle.putString("badgeType","OnTime_5");
+                intent.putExtras(bundle);
+                getContext().startActivity(intent);
+                break;
+            case  R.id.ImageView_show_up_on_time_10:
+                bundle.putString("badgeType","OnTime_10");
+                intent.putExtras(bundle);
+                getContext().startActivity(intent);
+                break;
+            case  R.id.ImageView_show_up_on_time_20:
+                bundle.putString("badgeType","OnTime_20");
+                intent.putExtras(bundle);
+                getContext().startActivity(intent);
+                break;
+            case  R.id.ImageView_show_up_on_time_50:
+                bundle.putString("badgeType","OnTime_50");
+                intent.putExtras(bundle);
+                getContext().startActivity(intent);
+                break;
+            case  R.id.ImageView_show_up_on_time_100:
+                bundle.putString("badgeType","OnTime_100");
+                intent.putExtras(bundle);
+                getContext().startActivity(intent);
+                break;
+
+            case R.id.ImageView_perfect_week:
+                bundle.putString("badgeType","Perfect_w");
+                intent.putExtras(bundle);
+                getContext().startActivity(intent);
+                break;
+            case R.id.ImageView_perfect_month:
+                bundle.putString("badgeType","Perfect_m");
+                intent.putExtras(bundle);
+                getContext().startActivity(intent);
+                break;
+            case R.id.ImageView_recommended_shift_1:
+                bundle.putString("badgeType","Recommended_1");
+                intent.putExtras(bundle);
+                getContext().startActivity(intent);
+                break;
+            case R.id.ImageView_recommended_shift_5:
+                bundle.putString("badgeType","Recommended_5");
+                intent.putExtras(bundle);
+                getContext().startActivity(intent);
+                break;
+            case R.id.ImageView_recommended_shift_10:
+                bundle.putString("badgeType","Recommended_10");
+                intent.putExtras(bundle);
+                getContext().startActivity(intent);
+
+                break;
         }
     }
 }
